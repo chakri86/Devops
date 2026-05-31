925c3780e5c80f97da92083b1d9bad67e9e5c523
chakri86 <chakravarthi.ananthu@gmail.com>
2026-05-31 17:36:33 -0500

    update

--- Shell_scripts/04-timestamp.sh
+++ Shell_scripts/04-timestamp.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-timestamp=$(date)
+# timestamp=$(date)
 
-echo "current timestamp: $timestamp"
+# echo "current timestamp: $timestamp"
 
-timestamp1=$(date +"%Y-%m-%d %H:%M:%S")
-echo "current timestamp in custom format: $timestamp1"
+# timestamp1=$(date +"%Y-%m-%d %H:%M:%S")
+# echo "current timestamp in custom format: $timestamp1"
+
+star_timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
 
+sleep 10
+
+
+endtimestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+total_time=$(date -d "$endtimestamp" +%s) - $(date -d "$star_timestamp" +%s)
+echo "total time taken: $total_time seconds"

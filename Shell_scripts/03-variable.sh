a398b0e08c52ba875a159c683d858a4f3f91f056
chakri86 <chakravarthi.ananthu@gmail.com>
2026-05-31 17:26:33 -0500

    update

--- Shell_scripts/03-variable.sh
+++ Shell_scripts/03-variable.sh
@@ -4,3 +4,7 @@ echo "please enter your name:"
 read username
 
 echo "hello $username, welcome to shell scripting!"
+
+echo "please enter your password:"
+read -s password
+echo "your password is: $password"

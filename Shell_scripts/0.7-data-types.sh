068623de57a5df206b31d7a5aed8d1402ee5fc1c
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-02 13:50:47 -0500

    update

--- Shell_scripts/0.7-data-types.sh
+++ Shell_scripts/0.7-data-types.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 num1=10
 num2=chakri
+num3=20
+
+
 echo "num1: $num1"
 echo "num2: $num2"
+echo "num3: $num3"
 
-sum=$((num1 + num2))
-
-echo "sum: $sum"
+sum1=$((num1 + num3))
+sum2=$((num1 + num2))
+sum3=$((num1+ num2 + num3))
+echo "sum1: $sum1"
+echo "sum2: $sum2"
+echo "sum3: $sum3"
 
 movies=("movie1" "movie2" "movie3")
 echo "movies: ${movies[@]}"

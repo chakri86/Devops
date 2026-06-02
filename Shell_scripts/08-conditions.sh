89eb69edc8bc9570a786e7e11da415d336a8b549
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-02 14:33:30 -0500

    update

--- Shell_scripts/08-conditions.sh
+++ Shell_scripts/08-conditions.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 number=$1
 
+#gt - greater than
+#lt - less than
+#eq - equal to 
+#ne - not equal to
+#ge - greater than or equal to
+#le - less than or equal to
+
+
 
 if [$number -gt 5 ]; then
     echo "number is greater than 5"
-elif [$number -gt 10 ]; then
-    echo "number is greater than 10"
 elif [$number -lt 5 ]; then
     echo "number is less than 5"
 else

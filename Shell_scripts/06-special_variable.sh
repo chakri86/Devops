aef430f85396094820663d164184ec406144a8f6
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-02 12:34:17 -0500

    update

---
+++ Shell_scripts/06-special_variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##special variables
+
+echo " all variables: passe to the scipts  $@"
+echo " total number of variables passed to the script: $#"
+echo " name of the script: $0"
+echo " first variable: $1"
+echo " second variable: $2"

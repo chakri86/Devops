d214c6d66271b81e7f5b2ce72c8933bdd4be56df
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-02 12:17:39 -0500

    update

---
+++ Shell_scripts/05-var_name.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+var1= $1
+var2= $3
+
+echo "var1: $var1"
+echo "var2: $var2"
+
+echo "var1: ${var1}"
+echo "var2: ${var2}"
+
+echo "var1: ${var1:-default_value}"
+echo "var2: ${var2:-default_value}"
+
+echo "hi $var1, how are you?"
+echo "hi $var2, how are you?"
+
+

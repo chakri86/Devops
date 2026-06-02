0c995e843c352d6c7fb15afc22852b6749204c3e
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-02 14:31:33 -0500

    update

--- Shell_scripts/08-conditions.sh
+++ Shell_scripts/08-conditions.sh
@@ -4,8 +4,8 @@ number=$1
 
 if [$number -gt 5 ]; then
     echo "number is greater than 5"
-elif [$number -lt 10 ]; then
-    echo "number is less than 10"
+elif [$number -gt 10 ]; then
+    echo "number is greater than 10"
 elif [$number -lt 5 ]; then
     echo "number is less than 5"
 else

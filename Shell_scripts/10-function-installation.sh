b4f4737a408c11665df657a3da9867b51655ba22
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-02 15:53:02 -0500

    update

---
+++ Shell_scripts/10-function-installation.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+#chekc if the user is root or not
+if [ $userid -eq 0 ]; then
+    echo "you are root user, you can install the software"
+else
+    echo "you are not root user, you cannot install the software"
+    exit 1
+fi
+
+validate(){
+    if [ $2 -eq 0 ]; then
+        echo "$1installed successfully"
+    else
+        echo "$1 installation failed"
+        exit 1
+    fi 
+}
+
+# myql installation
+
+dnf list installed mysql
+
+if [ $? -eq 0 ]; then
+    echo "software is already installed"
+    exit 0
+else
+    echo "software is not installed, installing the software..."
+    dnf install mysql -y
+
+    validate "mysql" $?
+fi
+
+# nginx installation
+
+dnf list installed nginx
+
+if [ $? -eq 0 ]; then
+    echo "software is already installed"
+    exit 0
+else
+    echo "software is not installed, installing the software..."
+    dnf install nginx -y
+
+    validate "nginx" $?
+fi

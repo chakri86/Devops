fdfc2570692deae9662d80aa173a49e4ef2ce9a8
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-02 15:19:08 -0500

    update

--- Shell_scripts/09-install.sh
+++ Shell_scripts/09-install.sh
@@ -3,7 +3,7 @@
 userid=$(id -u)
 
 #chekc if the user is root or not
-if [ userid -eq 0 ]; then
+if [ $userid -eq 0 ]; then
     echo "you are root user, you can install the software"
 else
     echo "you are not root user, you cannot install the software"

0f9a17b46eee60cbaa90271e62ecb01eb252d7cf
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-13 13:15:21 -0500

    update commit

--- Shell_scripts/12-loops.sh
+++ Shell_scripts/12-loops.sh
@@ -5,6 +5,8 @@ logs_dir=/home/ec2-user/logs  # log file directory
 
 log_file="$logs_dir/$0.log"
 
+time_stamp=$(date "+%Y-%b-%d %H:%M:%S %Z") # we are getting the current date and time and storing it in a variable
+
 
 
 #chekc if the user is root or not
@@ -18,9 +20,9 @@ fi
 #we keept his repeated code in a function and we will call the function whenever we need to validate the installation of the software
 validate(){
     if [ $2 -eq 0 ]; then
-        echo "$1installed successfully" | tee -a $log_file # we are redirecting the output of this command to the log file
+        echo "$time_stamp $1installed successfully" | tee -a $log_file # we are redirecting the output of this command to the log file
     else
-        echo "$1 installation failed" | tee -a $log_file # we are redirecting the output of this command to the log file
+        echo "$time_stamp $1 installation failed" | tee -a $log_file # we are redirecting the output of this command to the log file
         exit 1
     fi 
 }
@@ -30,9 +32,9 @@ do
     echo " installin $package "
     dnf list installed $package &>> $log_file # we are redirecting the output of this command to the log file
     if [ $? -eq 0 ]; then
-        echo "software is already installed" | tee -a $log_file # we are redirecting the output of this command to the log file
+        echo "$time_stamp software is already installed" | tee -a $log_file # we are redirecting the output of this command to the log file
     else
-        echo "software is not installed, installing the software..."
+        echo "$time_stamp software is not installed, installing the software..."
         dnf install $package -y &>> $log_file # we are redirecting the output of this command to the log file
         validate "Installing $package" $?
     fi

ca924e3bb022f8a9dff2f1602e9f0ea320eb3a15
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-14 11:49:54 -0500

    update commit

---
+++ shell-roboshop/01-mogodb.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+log_folder="/var/log/roboshop"
+sudo mkdir -p $log_folder
+sudo chown -R ec2-user:ec2-user $log_folder
+sudo chmod -R 755 $log_folder
+log_file="$log_folder/$0.log"
+
+usr_id=$(id -u)
+
+R="\e[31m" # Red color
+G="\e[32m" # Green color
+N="\e[0m"  # No color
+Y="\e[33m" # Yellow color
+time_stamnp=$(date "+%Y-%b-%d %H:%M:%S %Z")
+
+if [ $usr_id -ne 0 ]; then
+  echo -e "$time_stamnp [ERROR] ${R} Please run the script as root user or with sudo privileges.${N}" | tee -a $log_file
+  exit 1
+fi
+
+validate() {
+  if [ $1 -ne 0 ]; then
+    echo -e "$time_stamnp [ERROR] ${R} $2 failed..${N}" | tee -a $log_file
+    exit 1
+  else
+    echo -e "$time_stamnp [INFO] ${G} $2 succeeded.${N}" | tee -a $log_file
+  fi
+}
+
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $log_file
+
+validate $? "Copying MongoDB repo file"
+
+
+
+

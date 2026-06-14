04ea165423c96f6ddeaa8f31bdfbdcb5f081aac2
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-14 18:48:52 -0500

    update commit

--- shell-roboshop/05-catalogue.sh
+++ shell-roboshop/05-catalogue.sh
@@ -30,19 +30,3 @@ validate() {
 }
 
 
-cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo  
-validate $? "Copying RabbitMQ repo file"
-
-dnf install rabbitmq-server -y &>> $log_file
-validate $? "Installing RabbitMQ Server"
-
-
-systemctl enable rabbitmq-server 
-systemctl start rabbitmq-server
-validate $? "Starting and Enabling RabbitMQ service"
-
-rabbitmqctl add_user roboshop roboshop123 &>> $log_file
-validate $? "Creating RabbitMQ user"
-
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $log_file
-validate $? "Setting RabbitMQ user permissions"

6ac6853b94a85b7b2dd8944bf0cc3e335d3595d8
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-14 19:41:09 -0500

    update commit

--- shell-roboshop/05-catalogue.sh
+++ shell-roboshop/05-catalogue.sh
@@ -6,6 +6,7 @@ sudo mkdir -p $log_folder
 sudo chown -R ec2-user:ec2-user $log_folder
 sudo chmod -R 755 $log_folder
 log_file="$log_folder/$0.log"
+script_directory=$pwd
 
 usr_id=$(id -u)
 
@@ -48,8 +49,48 @@ if [ $? -ne 0 ]; then
   echo -e "$time_stamnp [INFO] ${Y} roboshop user already exists, skipping user creation.${N}" | tee -a $log_file
 fi 
 
+rm -rf /app &>> $log_file
+validate $? "Removing existing application directory if it exists"
+
 mkdir /app &>> $log_file
 validate $? "Creating application directory"
+
+rm -rf /tmp/catalogue.zip &>> $log_file
+validate $? "Removing existing application archive if it exists"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>> $log_file
+cd /app 
+unzip /tmp/catalogue.zip &>> $log_file
+validate $? "Extracting catalogue application code"
+
+cd /app
+npm install &>> $log_file
+validate $? "Installing catalogue application dependencies"
+
+rm -rf /etc/systemd/system/catalogue.service &>> $log_file
+validate $? "Removing existing catalogue systemd service file if it exists"
+
+cp $script_directory/catalogue.service /etc/systemd/system/catalogue.service &>> $log_file
+validate $? "Copying catalogue systemd service file"
+
+rm -rf /etc/yum.repos.d/mongo.repo &>> $log_file
+validate $? "Removing existing MongoDB repo file if it exists"
+
+cp $script_directory/mongo.repo /etc/yum.repos.d/mongo.repo &>> $log_file
+validate $? "Copying MongoDB repo file"
+
+dnf install mongodb-mongosh -y &>> $log_file
+validate $? "Installing MongoDB client"
+
+
+
+
+
+
+
+
+
+
 
 
 

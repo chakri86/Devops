c01fe05a12e6970488a9379e326038b45a7adf37
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-15 17:06:01 -0500

    update commit

--- shell-roboshop/roboshop-v2.sh
+++ shell-roboshop/roboshop-v2.sh
@@ -27,3 +27,31 @@ if [ "$action" != "create" ] && [ "$action" != "delete" ]; then
     echo "Usage: $0 [create|delete] [instance1] [instance2] ..." | tee -a $log_file
     exit 1
 fi
+
+get_instance_id() {
+    name=$1
+    aws ec2 describe-instances --filters "Name=tag:Name,Values=$name" "Name=instance-state-name,Values=running" --query 
+    "Reservations[0].Instances[0].InstanceId" --output text
+}
+
+
+
+for instance in $@
+do
+   insrance_id=$(get_instance_id $instance)
+   if [ "$action" == "create" ]; then
+       if [ "$insrance_id" == "None" ]; then
+           echo -e "$time_stamnp [INFO] $G : Creating instance roboshop-$instance $N" | tee -a $log_file
+           instance_id=$(aws ec2 run-instances \
+           --image-id $ami_id \
+           --instance-type t3.micro \
+           --security-groups roboshop-common roboshop-$instance\
+           --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
+           --query 'Instances[0].InstanceId' \
+           --output text
+           )
+           echo -e "$time_stamnp [INFO] $G : Instance roboshop-$instance created with ID: $instance_id $N" | tee -a $log_file
+       else
+       fi
+   fi
+done

7b3e5fd535889881c668cdea8dedfea5044bb157
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-16 01:38:05 -0500

    update commit

---
+++ shell-roboshop/09-payment.sh
@@ -0,0 +1,71 @@
+
+#!/bin/bash
+
+log_folder="/var/log/roboshop"
+sudo mkdir -p $log_folder
+sudo chown -R ec2-user:ec2-user $log_folder
+sudo chmod -R 755 $log_folder
+log_file="$log_folder/$0.log"
+script_directory=$PWD
+MYSQL_HOST=mysql.avkc.online
+
+usr_id=$(id -u)
+
+R="\e[31m" # Red color
+G="\e[32m" # Green color
+N="\e[0m"  # No color
+Y="\e[33m" # Yellow color
+time_stamnp=$(date "+%Y-%b-%d %H:%M:%S %Z")
+
+if [ $usr_id -ne 0 ]; then
+  echo -e "$time_stamnp [ERROR] ${R} Please run the script as root user or with sudo privileges.${N}" | tee -a $log_file
+  exit 1
+fi
+
+validate() {
+  if [ $1 -ne 0 ]; then
+    echo -e "$time_stamnp [ERROR] ${R} $2 failed..${N}" | tee -a $log_file
+    exit 1
+  else
+    echo -e "$time_stamnp [INFO] ${G} $2 succeeded.${N}" | tee -a $log_file
+  fi
+}
+
+
+dnf install python3 gcc python3-devel -y &>> $log_file
+validate $? "Installing Python3, GCC, and development headers"
+
+id roboshop &>> $log_file
+if [ $? -ne 0 ]; then
+  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $log_file
+  validate $? "Creating roboshop system user"
+ else
+  echo -e "$time_stamnp [INFO] ${Y} roboshop user already exists, skipping user creation.${N}" | tee -a $log_file
+fi 
+
+rm -rf /app &>> $log_file
+validate $? "Removing existing application directory if it exists"
+
+mkdir -p /app &>> $log_file
+validate $? "Creating application directory"
+
+rm -rf /tmp/payment.zip &>> $log_file
+validate $? "Removing existing application archive if it exists"
+
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip  &>> $log_file
+cd /app 
+unzip /tmp/payment.zip &>> $log_file
+validate $? "Extracting payment application code"
+
+pip3 install -r requirements.txt &>> $log_file
+validate $? "Installing payment application dependencies"
+
+cp $script_directory/payment.service /etc/systemd/system/payment.service &>> $log_file
+validate $? "Copying payment systemd service file"
+
+
+systemctl enable payment   &>> $log_file 
+systemctl restart payment  &>> $log_file
+validate $? "Starting and Enabling payment service"
+
+

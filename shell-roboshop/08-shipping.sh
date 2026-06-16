0b6668a6c1ab09ca95758668628a6357fcc0f884
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-16 01:20:20 -0500

    update commit

--- shell-roboshop/08-shipping.sh
+++ shell-roboshop/08-shipping.sh
@@ -68,12 +68,12 @@ validate $? "Installing MySQL client"
 
 mysql -h $MYSQL_HOST -u root -pRoboShop@1 -e "use cities" &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
-    VALIDATE $? "Data loaded"
+  mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql
+  mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql
+  mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql
+  VALIDATE $? "$time_stamnp [INFO] ${G} Data loaded successfully.${N}" | tee -a $log_file
 else
-    echo -e "$time_stamnp [INFO] ${Y} Data already loaded ... SKIPPING $N" | tee -a $log_file
+  echo -e "$time_stamnp [INFO] ${Y} Data already loaded ... SKIPPING $N" | tee -a $log_file
 fi
 
 

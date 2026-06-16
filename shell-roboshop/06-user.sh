634165e5da15a40fce35ae598c21d49d107407b5
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-15 23:15:30 -0500

    update commit

---
+++ shell-roboshop/06-user.sh
@@ -0,0 +1,111 @@
+
+#!/bin/bash
+
+log_folder="/var/log/roboshop"
+sudo mkdir -p $log_folder
+sudo chown -R ec2-user:ec2-user $log_folder
+sudo chmod -R 755 $log_folder
+log_file="$log_folder/$0.log"
+script_directory=$PWD
+
+usr_id=$(id -u)
+
+R="\e[31m" # Red color
+G="\e[32m" # Green color
+N="\e[0m"  # No color
+Y="\e[33m" # Yellow color
+time_stamnp=$(date "+%Y-%b-%d %H:%M:%S %Z")
+
+if [ $usr_id -ne 0 ]; then
+  echo -e "$time_stamnp [ERROR] ${R} Please run the script as root user or with sudo privileges.${N}" | tee -a $log_file
+  exit 1
+fi
+
+validate() {
+  if [ $1 -ne 0 ]; then
+    echo -e "$time_stamnp [ERROR] ${R} $2 failed..${N}" | tee -a $log_file
+    exit 1
+  else
+    echo -e "$time_stamnp [INFO] ${G} $2 succeeded.${N}" | tee -a $log_file
+  fi
+}
+
+
+
+
+dnf module disable nodejs -y &>> $log_file
+
+dnf module enable nodejs:20 -y &>> $log_file
+
+dnf install nodejs -y &>> $log_file
+validate $? "Installing NodeJS:20"
+
+
+id roboshop &>> $log_file
+if [ $? -ne 0 ]; then
+  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+  validate $? "Creating roboshop system user"
+ else
+  echo -e "$time_stamnp [INFO] ${Y} roboshop user already exists, skipping user creation.${N}" | tee -a $log_file
+fi 
+
+rm -rf /app &>> $log_file
+validate $? "Removing existing application directory if it exists"
+
+mkdir /app &>> $log_file
+validate $? "Creating application directory"
+
+rm -rf /tmp/catalogue.zip &>> $log_file
+validate $? "Removing existing application archive if it exists"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>> $log_file
+cd /app 
+unzip /tmp/catalogue.zip &>> $log_file
+validate $? "Extracting catalogue application code"
+
+npm install &>> $log_file
+validate $? "Installing catalogue application dependencies"
+
+rm -rf /etc/systemd/system/catalogue.service &>> $log_file
+validate $? "Removing existing catalogue systemd service file if it exists"
+
+cp $script_directory/catalogue.service /etc/systemd/system/catalogue.service &>> $log_file
+validate $? "Copying catalogue systemd service file"
+
+rm -rf /etc/yum.repos.d/mongo.repo &>> $log_file
+validate $? "Removing existing MongoDB repo file if it exists"
+
+cp $script_directory/mongo.repo /etc/yum.repos.d/mongo.repo &>> $log_file
+validate $? "Copying MongoDB repo file"
+
+dnf install mongodb-mongosh -y &>> $log_file
+validate $? "Installing MongoDB client"
+
+index=$(mongosh --host mongodb.avkc.online --eval 'db.getMongo().getDBNames().indexOf("catalouge")')
+
+if [ $index -lt 0 ]; then
+  mongosh --host mongodb.avkc.online </app/db/master-data.js &>> $log_file
+  validate $? "load catalogue database and collection with initial data"
+ else
+  echo -e "$time_stamnp [INFO] ${Y} MongoDB user and database for catalogue already exists, skipping creation.${N}" | tee -a $log_file
+fi
+
+systemctl enable catalogue &>> $log_file
+systemctl restart catalogue &>> $log_file
+validate $? "Starting and Enabling catalogue service"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

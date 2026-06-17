62538b2aeaf7acbeed30b69f98c631a9c5e1a08b
chakri86 <chakravarthi.ananthu@gmail.com>
2026-06-17 09:40:27 -0500

    update commit

---
+++ shell-roboshop-v2/02-redis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source ./cpmmon.sh
+
+dnf module disable redis -y &>> $log_file
+dnf module enable redis:7 -y &>> $log_file
+dnf install redis -y &>> $log_file
+validate $? "Installing Redis"
+
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+validate $? "Updating Redis bind IP address to allow remote connections and disabling protected mode"
+
+systemctl enable redis
+systemctl start redis
+validate $? "Starting and Enabling Redis service"
+
+
+print_total_time
---
+++ shell-roboshop-v2/common.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+log_folder="/var/log/roboshop"
+sudo mkdir -p $log_folder
+sudo chown -R ec2-user:ec2-user $log_folder
+sudo chmod -R 755 $log_folder
+log_file="$log_folder/$0.log"
+
+usr_id=$(id -u)
+
+R="\e[31m" # Red color
+G="\e[32m" # Green color
+N="\e[0m"  # No color
+Y="\e[33m" # Yellow color
+time_stamnp=$(date "+%Y-%b-%d %H:%M:%S %Z")
+
+echo -e "$time_stamp [INFO] script started"
+
+check_root(){
+    if [ $usr_id -ne 0 ]; then
+        echo -e "$time_stamnp [ERROR] ${R} Please run the script as root user or with sudo privileges.${N}" | tee -a $log_file
+        exit 1
+    fi
+}
+
+
+validate() {
+  if [ $1 -ne 0 ]; then
+    echo -e "$time_stamnp [ERROR] ${R} $2 failed..${N}" | tee -a $log_file
+    exit 1
+  else
+    echo -e "$time_stamnp [INFO] ${G} $2 succeeded.${N}" | tee -a $log_file
+  fi
+}
+
+print_total_time(){
+    echo -e "$time_stamp [INFO] script executed in $G $SECONDS seconds $N"
+}
